7aa75b2f3b25ad83b608a0fe57374aca1e521616
Josh <taylorj12@uni.coventry.ac.uk>
2023-05-31 18:02:51 +0100

    class is setup just testing out how the frames are showed, functions can be used etc

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from video import VideoFormatter
 def main():
-    pass
+    video = VideoFormatter('C:/Users/Josh_2/Desktop/Venice_10.mp4')
+    video.getFrames()
+    video.showFrames()
 
 if __name__ == "__main__":
     main()
--- video.py
+++ video.py
@@ -11,24 +11,52 @@ import subprocess
 class VideoFormatter:
     def __init__(self, video_path):
         self.video_path = video_path
+        self.image_data = []
         self.video_name = self.getVideoName()
         self.convertVideoFormat()
+        self.cap = cv2.VideoCapture(self.video_path)
 
     def getVideoName(self):
         file_name = os.path.basename(self.video_path)
-        return os.path.splittext(file_name)[0]
+        return os.path.split(file_name)[0]
 
     def getFramesNumber(self):
-        #will get number of frames in the video
-        pass
-
+        return self.cap.get(cv2.cv2.CAP_PROP_FRAME_COUNT)
+        self.cap.release()
+    
+    def getHeightAndWidth(self):
+        return self.cap.get(cv2.cv2.CAP_PROP_FRAME_HEIGHT), self.cap.get(cv2.cv2.CAP_PROP_FRAME_WIDTH)
+        self.cap.release()
+    
     def getVideoLength(self):
-        #gets length of the video
-        pass
+        fps = self.getFPS()
+        numberOfFrames = self.getFramesNumber()
+        return numberOfFrames // fps
 
+    def display_cv2_img(img, figsize=(10,10)):
+        img_ = cv2.cvtColor(img, cv2.COLORBGR2RGB)
+        fig, ax = plt.subplots(figsize=figsize)
+        ax.imshow(img_)
+        ax.axis("off")
+    
+    def display_image(self, img):
+        self.display_cv2_img(img)
+
+    def getFPS(self):
+        return self.cap.get(cv2.CAP_PROP_FPS)
+    
     def getFrames(self, frames):
-        #gets 'frames' number of frames from the video and returns them
-        pass
+        number_of_frames = self.getFramesNumber()
+        jump = number_of_frames // frames
+        position = 0
+        for frame in range(frames):
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, position)
+            ret, img = self.cap.read()
+            position += jump
+            if ret == False:
+                break
+            self.image_data.append(img)
+        return self.image_data
 
     def convertVideoFormat(self):
         subprocess.run(['ffmpeg', '-i', self.video_path, 'qscale', '0', f'{self.video_name}.mp4'])
@@ -36,3 +64,13 @@ class VideoFormatter:
 
     def showVideo(self):
         ipd.Video(self.video_path, width=700)
+    
+    def showFrames(self):
+        fig, axs = plt.subplots(5, 5, figsize=(30,20))
+        axs = axs.flatten()
+        plt.show()
+        for frame in range(len(self.image_data)):
+            axs[frame].imshow(cv2.cvtColor(self.image_data[frame], cv2.COLOR_BGR2RGB))
+        plt.tight_layout()
+        plt.show()
+
