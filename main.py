85872cd5c4c38e92efd57acddefb5dc435f38c9e
Josh <taylorj12@uni.coventry.ac.uk>
2023-05-31 22:51:48 +0100

    still formatting all the data that will be passed to the convolutional network

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from formatting.video import VideoFormatter
 from model.train_model import TrainModel
 def main():
     model = TrainModel("squat")
-    model.trainCategorically()
+    model.create_training_data()
+    print(model.length_of_training())
 
 if __name__ == "__main__":
     main()
--- model/train_model.py
+++ model/train_model.py
@@ -3,19 +3,40 @@ import matplotlib.pyplot as plt
 import os
 import cv2
 import formatting.image as fi
+import random
 
 class TrainModel:
     def __init__(self, exercise):
         self.exercise = exercise
-        self.datadir = f"C:/Users/Josh_2/Desktop/VideoFormatter/training_images/{self.exercise}"
+        self.datadir = f"C:/Users/Josh_2/Desktop/VideoFormatter/training_images/{self.exercise}/"
         self.categories = ["good", "bad"]
-    
-    def trainCategorically(self):
+        self.training_data = []
+
+    def create_training_data(self):
         for category in self.categories:
             path = os.path.join(self.datadir, category)
+            class_num = self.categories.index(category)
             for img in os.listdir(path):
-                img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-                formatter = fi.ImageFormatter(img_array)
-                img_array = formatter.getFormattedImages()
-                break
-            break
+                try:
+                    img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+                    formatter = fi.ImageFormatter(img_array)
+                    new_array = formatter.getFormattedImages()
+                    self.training_data.append([new_array, class_num])
+                except Exception as e:
+                    print(e)
+    
+    def format_training_data(self):
+        X = []
+        y = []
+        for features, label in self.training_data:
+            X.append(features)
+            y.append(label)
+        formatter = fi.ImageFormatter([])
+        image_size = formatter.getImageSize()
+        X = np.array(X).reshape(-1, image_size, image_size, 1)
+
+    def shuffle_training_data(self):
+        random.shuffle(self.training_data)
+
+    def length_of_training(self):
+        return len(self.training_data)
