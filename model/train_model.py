b064a8a3c7f1a4b303da46954d107266ff6b8d67
Josh <taylorj12@uni.coventry.ac.uk>
2023-05-31 22:19:56 +0100

    images now formatted and will now move on to training

--- model/train_model.py
+++ model/train_model.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import cv2
-import source.formatting
+import formatting.image as fi
 
 class TrainModel:
     def __init__(self, exercise):
@@ -15,7 +15,7 @@ class TrainModel:
             path = os.path.join(self.datadir, category)
             for img in os.listdir(path):
                 img_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
-                formatter = ImageFormatter(img_array)
+                formatter = fi.ImageFormatter(img_array)
                 img_array = formatter.getFormattedImages()
                 break
             break
